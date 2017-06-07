2cde0ea4dd18d33995a98ac11e33cbb82b911c2a
Angelika Pacek <angelika.pacek@synapse.com>
2017-06-07 11:43:41 +0200

    setFieldToFill

--- sudoku.cpp
+++ sudoku.cpp
@@ -21,32 +21,42 @@ void Sudoku::loadData()
           exit(1); // do zmiany
        }
 
-       for (int column = 0; column < 9; column++)
+       std::cout << "oryginalna tablica:\n";
+       for (int row = 0; row < 9; row++)
        {
-          for (int row = 0; row < 9; row++)
+          for (int column = 0; column < 9; column++)
           {
              board >> sudokuBoard[row][column];
+             std::cout << sudokuBoard[row][column];
           }
+          std::cout << "\n";
 
        }
        board.close();
 
-       for (int column = 0; column < 9; column++)
+       std::cout << "\n";
+       std::cout << "modyfikowalna tablica:\n";
+       for (int row = 0; row < 9; row++)
        {
-          for (int row = 0; row < 9; row++)
+          for (int column = 0; column < 9; column++)
           {
              modifiableSudokuBoard[row][column] = sudokuBoard[row][column];
+             std::cout << modifiableSudokuBoard[row][column];
           }
+          std::cout << "\n";
        }
+       std::cout << "\n";
 }
 
 void Sudoku::drawBoard()
 {
   std::string sudokuBoardString[9][9];
 
-  for (int column = 0; column < 9; column++)
+
+
+  for (int row = 0; row < 9; row++)
   {
-     for (int row = 0; row < 9; row++)
+     for (int column = 0; column < 9; column++)
      {
         sudokuBoardString[row][column] = std::to_string(modifiableSudokuBoard[row][column]);
         if (sudokuBoardString[row][column] == "0")
@@ -60,20 +70,20 @@ void Sudoku::drawBoard()
     std::cout << "   A B C D E F G H I" << std::endl << std::endl;
     for (int row = 0; row < 9; row++)
     {
-          int printedRow = row+1;
-          std::cout << printedRow << "  "
-           << sudokuBoardString[0][row]
-           << " " << sudokuBoardString[1][row]
-           << " " << sudokuBoardString[2][row]
-           << "|"
-           << sudokuBoardString[3][row]
-           << " " << sudokuBoardString[4][row]
-           << " " << sudokuBoardString[5][row]
-           << "|"
-           << sudokuBoardString[6][row]
-           << " " << sudokuBoardString[7][row]
-           << " " << sudokuBoardString[8][row]
-           << std::endl;
+      int printedRow = row+1;
+      std::cout << printedRow << "  "
+       << sudokuBoardString[row][0]
+       << " " << sudokuBoardString[row][1]
+       << " " << sudokuBoardString[row][2]
+       << "|"
+       << sudokuBoardString[row][3]
+       << " " << sudokuBoardString[row][4]
+       << " " << sudokuBoardString[row][5]
+       << "|"
+       << sudokuBoardString[row][6]
+       << " " << sudokuBoardString[row][7]
+       << " " << sudokuBoardString[row][8]
+       << std::endl;
 
            if (row == 2 || row == 5 )
            {
@@ -88,8 +98,29 @@ std::pair<int, int> Sudoku::setFieldToFill()
 
   int row;
   int column;
+  bool isPosible = true;
+
+  std::cout << "\noryginalna tablica przed do while:\n";
+  for (int row = 0; row < 9; row++)
+  {
+     for (int column = 0; column < 9; column++)
+     {
+        std::cout << sudokuBoard[row][column] << " ";
+     }
+     std::cout << "\n";
+   }
 
   do {
+
+    std::cout << "\noryginalna tablica w do while:\n";
+    for (int row = 0; row < 9; row++)
+    {
+       for (int column = 0; column < 9; column++)
+       {
+          std::cout << sudokuBoard[row][column] << " ";
+       }
+       std::cout << "\n";
+     }
 
     char columnChar;
 
@@ -100,20 +131,44 @@ std::pair<int, int> Sudoku::setFieldToFill()
 
     columnChar = std::toupper(columnChar);
 
-    std::cout << columnChar << "kolumna   \n";
-    std::cout << row << "rzad   \n";
+    // std::cout << columnChar << "kolumna   \n";
+    // std::cout << row << "rzad   \n";
     column = columnChar - 64;
 
-    std::cout << column << "kolumna po konwersji   \n";
-    std::cout << row << "rzad   \n";
+    // std::cout << column << "kolumna po konwersji   \n";
+    // std::cout << row << "rzad   \n";
 
-  } while(!(row >= 1 && row <= 9 && column >=1 && column <= 9));
+    if ((sudokuBoard[row - 1][column - 1]) != 0)
+    {
+     std::cout << "niemodyfikowalne pole\n";
+     std::cout << sudokuBoard[row - 1][column - 1] << "w tablicy jest \n";
+     isPosible = false;
+    }
+
+
+    std::cout << "row: " << row << " \n";
+    std::cout << "column: " << column << " \n";
+    std::cout << "isPosible: " << isPosible << " \n";
+
+  } while ((isPosible == false) || (row < 1) || (row > 9) || (column < 1) || (column > 9));
 
   field.first = row - 1;
   field.second = column - 1;
 
-  std::cout << field.first << "   ";
-  std::cout << field.second;
+  std::cout << "\noryginalna tablica po do while:\n";
+  for (int row = 0; row < 9; row++)
+  {
+     for (int column = 0; column < 9; column++)
+     {
+        std::cout << sudokuBoard[row][column] << " ";
+     }
+     std::cout << "\n";
+   }
+
+  std::cout << field.first << " row\n";
+  std::cout << field.second << " column\n";
+  std::cout << sudokuBoard[field.first][field.second] << " w oryginalnej tablicy w w polu [" << field.first << "][" <<field.second<<"]\n";
+
   return field;
 }
 
